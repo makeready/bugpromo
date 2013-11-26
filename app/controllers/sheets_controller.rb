b149cda931ec8b93c8ca11f7c792d6829d6367ab
make ready <shotsinthedark@gmail.com>
2013-11-26 01:39:48 -0500

    CSV tightened up and existing values properly y selected in sheet#edit

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -14,12 +14,13 @@ class SheetsController < ApplicationController
 	def create
 		@sheet = current_user.sheets.build(sheet_params)
 		@sheet.save
-		update_property
+		@property = Property.find_or_create_by(name: @sheet.property_name)
+		update_property(@property)
 		update_teamnames
 
-		update_tbl_spec(@sheet.weekday1)
-		update_tbl_spec(@sheet.weekday2)
-		update_tbl_spec(@sheet.weekday3)
+		update_tbl_spec(@sheet.weekday1, @property)
+		update_tbl_spec(@sheet.weekday2, @property)
+		update_tbl_spec(@sheet.weekday3, @property)
 
 		
 		update_ae_spec	
@@ -27,19 +28,22 @@ class SheetsController < ApplicationController
 		redirect_to sheets_path
 	end
 
-	def update_tbl_spec(weekday)
+	def update_tbl_spec(weekday, property)
 		unless weekday.empty?
 			#CREATE TBL SPEC  --- CSV FILE
 			@tbl_spec = TblSpec.find_or_create_by(sheet_id: @sheet.id, day: weekday)
 			@tbl_spec.sheet_id = @sheet.id
 			@tbl_spec.day = weekday
-			@tbl_spec.image_path = @sheet.property.image_path
+			@tbl_spec.image_path = property.image_path
 			@tbl_spec.property_name = @sheet.property_name
 			@tbl_spec.team1 = @sheet.team1
 			@tbl_spec.team2 = @sheet.team2
 			@tbl_spec.time_variable = ""
-			@sheet.airtimes.each do |airtime|
-				@tbl_spec.time_variable += (airtime.time.to_s + " " + airtime.timezone + ";")
+			@sheet.airtimes.each_with_index do |airtime, i|
+				if airtime.time
+					@tbl_spec.time_variable += airtime.time.hour.to_s + ":" + airtime.time.min.to_s + " " + airtime.timezone
+					@tbl_spec.time_variable += " / " unless @sheet.airtimes[i+1].time == nil
+				end
 			end
 			@tbl_spec.world = @sheet.world
 			@tbl_spec.east = @sheet.east
@@ -47,7 +51,7 @@ class SheetsController < ApplicationController
 			@tbl_spec.west = @sheet.west
 			@tbl_spec.pacific = @sheet.pacific
 			@tbl_spec.one = @sheet.one
-			@tbl_spec.logo_path = @sheet.property.logo_path
+			@tbl_spec.logo_path = property.logo_path
 			@tbl_spec.save 
 		end
 	end
@@ -62,11 +66,10 @@ class SheetsController < ApplicationController
 		end
 	end
 
-	def update_property
+	def update_property(property)
 		#ADDS NEW ENTRY TO PROPERTY TABLE IF NAME DOES NOT EXIST
-		@property = Property.find_or_create_by(name: @sheet.property_name)
-		@property.save
-		@sheet.property_id = @property.id
+		property.save
+		@sheet.property_id = property.id
 	end
 
 	def update_teamnames
@@ -86,10 +89,14 @@ class SheetsController < ApplicationController
 
 	def update
 		@sheet = Sheet.find(params[:id])
-
-		update_property
+		@property = Property.find_or_create_by(name: @sheet.property_name)
+		update_property(@property)
 		update_teamnames
-		update_tbl_spec
+
+		update_tbl_spec(@sheet.weekday1, @property)
+		update_tbl_spec(@sheet.weekday2, @property)
+		update_tbl_spec(@sheet.weekday3, @property)
+
 		update_ae_spec
 
 		if @sheet.update_attributes(sheet_params)
--- app/models/tbl_spec.rb
+++ app/models/tbl_spec.rb
@@ -3,9 +3,9 @@ class TblSpec < ActiveRecord::Base
 
 	def self.to_csv
     CSV.generate do |csv|
-      csv << column_names
+      csv << column_names[4..-1]
       all.each do |sheet|
-        csv << sheet.attributes.values_at(*column_names)
+        csv << sheet.attributes.values_at(*column_names[4..-1])
       end
     end
   end
